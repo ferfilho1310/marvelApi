2ca5b4a51562e4117b8b0fa264d9eb4c7b69b4ed
Fernando <fernandofilho1310@gmail.com>
2021-08-19 17:03:59 -0300

    Implementação para consumo da API Marvel utilizando Courotines, Clean Architecture e Koin.

--- app/src/main/java/com/example/marvelcomics/Retrofit/Retrofit.kt
+++ app/src/main/java/com/example/marvelcomics/Retrofit/Retrofit.kt
@@ -11,7 +11,6 @@ const val publicKey = "8471dc482b6dd1140e8bb074b940b8af"
 const val timesTampApiUrl = "1629245895"
 
 var URL = "https://gateway.marvel.com:443/v1/public/"
-var URL_1 = "https://covid19-brazil-api.now.sh/api/report/v1/"
 
 fun getRetrofit(): Retrofit? {
     retrofit = Retrofit.Builder()
